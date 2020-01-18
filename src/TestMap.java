0d23d3034f738bc24f62872af68da8e75db738dd
matbiec1102 <biettuot505@gmail.con>
2020-01-18 11:03:48 +0700

    done

---
+++ src/TestMap.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class TestMap {
+    public static void main(String[] args) {
+        Map<String,Integer> hasMap = new HashMap<>();
+        hasMap.put("Smith",30);
+        hasMap.put("Anderson",31);
+        hasMap.put("Lewis",29);
+        hasMap.put("Cook",29);
+        System.out.println("Display entries in HashMap:");
+        System.out.println(hasMap + "\n");
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("Smith",30);
+        linkedHashMap.put("Anderson",31);
+        linkedHashMap.put("Lewis",29);
+        linkedHashMap.put("Cook",29);
+        System.out.println("\nThe age for "+"Lewis is "+ linkedHashMap.get("Lewis"));
+    }
+}
